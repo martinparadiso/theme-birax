6d02f1b91aad86770eac8f0a6b9bfb68b506dc86
Martín Paradiso <martinparadiso@outlook.com>
2023-10-25 21:06:50 -0300

    feat: merge changes from custom/private version

--- README.md
+++ README.md
@@ -1,11 +1,84 @@
-## bira
+<img src="https://cdn.rawgit.com/oh-my-fish/oh-my-fish/e4f1c2e0219a17e2c748b824004c8d0b38055c16/docs/logo.svg" align="left" width="144px" height="144px"/>
 
-Theme imported from Oh my ZSH: https://github.com/robbyrussell/oh-my-zsh/blob/master/themes/bira.zsh-theme
-Contains traces of nuts and code from other Oh my fish themes.
+#### birax
+> A theme for [Oh My Fish][omf-link].
 
-Display username, hostname, current path, ruby version and git status. Display the latest error status in the right prompt.
+[![MIT License](https://img.shields.io/badge/license-MIT-007EC7.svg?style=flat-square)](/LICENSE)
+[![Fish Shell Version](https://img.shields.io/badge/fish-v3.0.0-007EC7.svg?style=flat-square)](https://fishshell.com)
+[![Oh My Fish Framework](https://img.shields.io/badge/Oh%20My%20Fish-Framework-007EC7.svg?style=flat-square)](https://www.github.com/oh-my-fish/oh-my-fish)
 
-![bira theme](https://cloud.githubusercontent.com/assets/1214238/5441541/8bd494dc-8491-11e4-9dbe-256b7e558eac.png)
+<br/>
 
-TODO:
- - display Python version
+Fork of [om-my-fish](https://github.com/oh-my-fish/theme-bira) with some extra
+sparkle.
+
+## Install
+
+```fish
+$ omf install github.com/martinparadiso/theme-birax
+```
+
+**NOTE**: The theme uses [Nerd Fonts](https://www.nerdfonts.com/) by default,
+if you see strange characters either install a patched font, or replace the icons with:
+
+```fish
+set -U birax_home_icon '~'
+set -U birax_git_icon ''
+set -U birax_shell_icon '$ '
+set -U birax_private_icon '(private)'
+```
+
+## Features
+
+- 'Nice path' mode by setting an Universal variable `birax_nice_path`
+- Hide the 'pipes' by setting `birax_no_pipes`
+- Python VirtualEnv version
+- Private mode icon
+- Experimental support for Vi mode
+
+## Screenshot
+
+<p align="center">
+<img src="./images/example.png">
+</p>
+
+# Usage
+
+You can tweak the following environment variables to customize the theme.
+'Flag' variables are 'active' if they are present with any value (can be
+empty). 'text' variables are used only if present, and the text given is
+used in the corresponding place, can be empty to remove the icon.
+
+| Variable              | Type      | Notes                                                     |
+|-----------------------|-----------|-----------------------------------------------------------|
+| `birax_nice_path`     | flag      | Replace fish std. path function with something nicer.     |
+| `birax_no_pipes`      | flag      | Remove the 'pipes' connecting the prompt lines            |
+| `birax_home_icon`     | text      | Set the icon for the `HOME` directory in nice path mode   |
+| `birax_home_git`      | text      | Set the icon to prepend to current `git` checkout         |
+| `birax_shell_icon`    | text      | Set the icon for shell prompt                             |
+| `birax_private_icon`  | text      | Set the icon to indicate the shell is in private mode     |
+
+
+To change an icon or activate a flag:
+
+```fish
+set -U <variable> <optional-content>
+```
+
+To disable a flag or go back to default icons:
+
+```fish
+set -Ue <variable>
+```
+p
+# License
+
+[MIT][mit] © [Martín Paradiso][author] et [al][contributors]
+
+
+[mit]:            https://opensource.org/licenses/MIT
+[author]:         https://github.com/martinparadiso
+[contributors]:   https://github.com/martinparadiso/theme-birax/graphs/contributors
+[omf-link]:       https://www.github.com/oh-my-fish/oh-my-fish
+
+[license-badge]:  https://img.shields.io/badge/license-MIT-007EC7.svg?style=flat-square
--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,18 +1,43 @@
 # Theme based on Bira theme from oh-my-zsh: https://github.com/robbyrussell/oh-my-zsh/blob/master/themes/bira.zsh-theme
 # Some code stolen from oh-my-fish clearance theme: https://github.com/bpinto/oh-my-fish/blob/master/themes/clearance/
 
-function __user_host
-  set -l content 
+set __birax_home_icon (set --query birax_home_icon; or echo ' ')
+set __birax_git_icon (set --query birax_git_icon; or echo '')
+set __birax_shell_icon (set --query birax_shell_icon; or echo '❭ ')
+set __birax_private_icon (set --query birax_private_icon; or echo '󰗹')
+set current_mode_color normal
+
+function __birax_print_userhost 
   if [ (id -u) = "0" ];
     echo -n (set_color --bold red)
   else
     echo -n (set_color --bold green)
   end
-  echo -n $USER@(hostname|cut -d . -f 1) (set color normal)
+  if [ -n "$SSH_CONNECTION" ]
+    set _hostname @(hostname|cut -d . -f 1)
+  else
+    set _hostname ""
+  end
+  echo -n $USER$_hostname (set color normal)
+end
+
+function __birax_print_incognito
+  if [ -n "$fish_private_mode" ]
+    echo -n " $__birax_private_icon "
+  end
 end
 
 function __current_path
-  echo -n (set_color --bold blue) (pwd) (set_color normal) 
+  set -l currpath
+  if set --query birax_nice_path
+    set currpath (pwd)
+    set currpath (string replace --regex "^$HOME" "$__birax_home_icon" "$currpath")
+    set currpath (string replace --all "/" '  ' "$currpath")
+    set currpath (string replace --regex "^  " '/  ' "$currpath")
+  else
+    set currpath (prompt_pwd -d 1 -D 1)
+  end
+  echo -n (set_color --bold blue) $currpath (set_color normal) 
 end
 
 function _git_branch_name
@@ -23,45 +48,72 @@ function _git_is_dirty
   echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
 end
 
-function __git_status
+
+function __birax_print_git_status
   if [ (_git_branch_name) ]
-    set -l git_branch (_git_branch_name)
+    set --local git_branch (_git_branch_name)
 
     if [ (_git_is_dirty) ]
-      set git_info '<'$git_branch"*"'>'
+      set git_color yellow
     else
-      set git_info '<'$git_branch'>'
+      set git_color green
     end
-
-    echo -n (set_color yellow) $git_info (set_color normal) 
+    echo -n (set_color $git_color) $__birax_git_icon $git_branch (set_color normal)
   end
 end
 
-function __ruby_version
-  if type "rvm-prompt" > /dev/null 2>&1
-    set ruby_version (rvm-prompt i v g)
-  else if type "rbenv" > /dev/null 2>&1
-    set ruby_version (rbenv version-name)
-  else
-    set ruby_version "system"
+function __birax_print_venv
+  set venv ''
+  if [ -n "$VIRTUAL_ENV" ]
+    set venv (python --version)
+    set __venv_icon 
   end
 
-  echo -n (set_color red) ‹$ruby_version› (set_color normal)
+  [ -n "$venv" ] && echo -n (set_color red) $__venv_icon $venv (set_color normal)
+end
+
+
+set -q color_vi_mode_normal; or set color_vi_mode_normal blue
+set -q color_vi_mode_insert; or set color_vi_mode_insert green
+set -q color_vi_mode_visual; or set color_vi_mode_visual magenta
+set -q color_vi_mode_replace; or set color_vi_mode_replanormalce red
+
+function __mode
+  switch $fish_bind_mode
+      case default
+        set -g current_mode_color $color_vi_mode_normal 
+        set -g current_mode_text "N"
+      case insert
+        set -g current_mode_color $color_vi_mode_insert
+        set -g current_mode_text "I"
+      case visual
+        set -g current_mode_color $color_vi_mode_visual
+        set -g current_mode_text "V"
+      case replace
+        set -g current_mode_color $color_vi_mode_replace
+        set -g current_mode_text "R"
+    end
+  echo -n (set_color --bold $current_mode_color)$current_mode_text (set_color normal)
 end
 
 function fish_prompt
-  echo -n (set_color white)"╭─"(set_color normal)
-  __user_host
+  set --query birax_no_pipes; or echo -n (set_color white)"╭─"(set_color normal)
+  if not test "$fish_key_bindings" = "fish_default_key_bindings"
+    set cursor_color $current_mode_color
+    __mode
+  end
+  __birax_print_userhost
+  __birax_print_incognito
   __current_path
-  __ruby_version
-  __git_status
+  __birax_print_venv
+  __birax_print_git_status
   echo -e ''
-  echo (set_color white)"╰─"(set_color --bold white)"\$ "(set_color normal)
+  set -q birax_no_pipes; or echo -n (set_color white)"╰─"
+  echo -n (set_color $current_mode_color)$__birax_shell_icon(set_color normal)
 end
 
 function fish_right_prompt
   set -l st $status
-
   if [ $st != 0 ];
     echo (set_color red) ↵ $st(set_color normal)
   end
